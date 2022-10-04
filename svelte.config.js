cfd4ff10e13cf5fa5df81fb741102a13b4e1da11
Deep <99deepsundar@gmail.com>
2022-10-04 22:53:57 +0530

    My first SvelteKIt app

---
+++ svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	},
+	// for enable postcss
+	preprocess: [
+		preprocess({
+		  postcss: true,
+		}),
+	  ],
+};
+
+export default config;
