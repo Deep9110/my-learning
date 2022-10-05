e03bef4a761e321d4420ceade6c9c6d08054352a
Deep <99deepsundar@gmail.com>
2022-10-05 15:16:21 +0530

    Order track updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,87 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
+<!-- <div class="text-red-500">Welcomet </div> -->
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<div class="flex items-center justify-center h-screen">
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
 
-		to your new<br />SvelteKit app
-	</h1>
+<nav class="navmenu absolute translate-y-2/4 p-6 bg-white-500 shadow-lg">
+	<!-- <ul class="sticky pl-[50px]  after:content-[''] after:absolute after:w-[2px] after:h-[calc(100% - 58px)] after:left-[36px]
+                after:bg-green-500 after:mt-[29px] after:z-[1]"> -->
+    <ul class=" pl-[73px] relative">
+		<li class="flex flex-col py-4">	
+			<a href=" " class="active relative text-lg font-medium text-[#1f1f1f]">Order Confirmed</a>
+			<span class="active text-gray-500">Fri, 30th Sep 22</span>
+		</li>
+		<li class="flex flex-col py-4">
+			<!-- <div>
+                <div class="">
+                    <div class="bg-black w-[6px] h-[6px] rounded">.</div>
+                </div>
+                <div>
+                    <div class="line">-</div>
+                </div>
+            </div> -->
+            <!-- <div> -->
+                <a href=" " class="relative text-lg font-medium text-[#1f1f1f]">Shipped</a>
+                <span class="text-gray-500">Sat, 1st Ot 22</span>
+            <!-- </div> -->
+			
+			
+		</li>
+		<li class="flex flex-col py-4">
+			
+			<a href=" " class="text-lg no-underline relative font-medium text-[#1f1f1f]">Out For Delivery</a>
+			<span class="text-gray-500"></span>
+		</li>
+		<li class="flex flex-col py-4">
+			
+			<a href=" " class="relative text-lg font-medium text-[#1f1f1f]">Delivery<i class="fa-solid fa-greater-than"></i></a>
+            <span class="text-gray-500">Item yet to be delivered.</span>
+		</li>
+	</ul>
+</nav>
+</div>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+    .navmenu ul::after{
+    content: "";
+    position:absolute;
+    width: 2px;
+    height: calc(100% - 78px);
+    left: 36px;
+    top: 0;
+    background: rgb(118, 196, 118);
+    z-index: -1;
+    margin-top: 29px;
+}   
+.navmenu ul li a:before{
+    content: '';
+    position: absolute;
+    background: #fff;
+    width:12px;
+    height: 12px;
+    left: -42px;
+    top: 50%;
+    transform: translateY(-50%);
+    border-radius: 50px;
+    border: 3px solid rgb(118, 196, 118);
+    transition: background-color 250ms linear;
+}
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+.navmenu ul li a.active{
+    font-weight: 600;
+}
+.navmenu ul li span.active{
+    font-weight: 600;
+}
+.navmenu ul li a.active::before{
+    background:green;
+}
+/* .navmenu ul.active2::after{
+    background:green;
+} */
+.navmenu ul li a.active:before{
+    border: 3px solid green;
+}
 </style>
+
